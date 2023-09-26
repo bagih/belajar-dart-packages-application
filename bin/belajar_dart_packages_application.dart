de9b780e2205b0752482b712af4892b6dce47e08
Bagi Hartawan <hartawanbagie@gmail.com>
2023-09-27 01:09:46 +0800

    initial commit

---
+++ bin/belajar_dart_packages_application.dart
@@ -0,0 +1,8 @@
+// import 'package:belajar_dart_packages_application/belajar_dart_packages_application.dart'
+// as belajar_dart_packages_application;
+import 'package:belajar_dart_package/hello.dart';
+
+void main(List<String> arguments) {
+  // print('Hello world: ${belajar_dart_packages_application.calculate()}!');
+  print(sayHello('bagih'));
+}
---
+++ bin/import_as.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_dart_package/hello.dart' as belajar;
+
+void main() {
+  print(belajar.sayHello('bagih'));
+}
